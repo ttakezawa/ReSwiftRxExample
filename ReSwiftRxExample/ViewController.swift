13192f90e3ad6a1ec42cb533f1d981a8837cd79a
Tomohiro Takezawa <ttakezawa@users.noreply.github.com>
2016-10-12 21:57:17 +0900

    Initial Commit

---
+++ ReSwiftRxExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReSwiftRxExample
+//
+//  Created by Tomohiro Takezawa on 2016/10/16.
+//  Copyright © 2016年 Tomohiro Takezawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
